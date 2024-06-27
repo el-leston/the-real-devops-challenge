cc79f40d481c72542278880434616d4865779642
el-surface <emanuelleston@gmail.com>
2024-06-27 23:53:06 +0100

    uncommented modules

--- challenge-3/02_main.tf
+++ challenge-3/02_main.tf
@@ -6,22 +6,24 @@
   subnets = var.subnets
 
 }
- 
+
 module "asg" {
   source = "./aws-modules/asg"
-  #ASG scales towards n+1 private subnetes
   region     = var.region
   cidrs      = var.cidrs
   subnet_ids = module.vpc.private_subnet_ids
   default_sg = module.vpc.default_sg_id
+  private_routable_id = module.vpc.private_routable_id
+  nat_subnet_id = module.vpc.public_subnet_a_id
   vpc_id     = module.vpc.vpc_id
   asg_name   = var.asg_name
   
   depends_on = [module.vpc]
 
-} /*
+}
+  
  module "nlb" {
-  #Network load balancer targeting ASG public subnet A and private subnet B
+  #Network load balancer targeting ASG public subnet A and private subnet B, C
   #receives outside traffic towards ASG in the private subnet
   source        = "./aws-modules/nlb"
   subnet_ids    = module.vpc.nlb_subnets_ids
@@ -34,7 +36,6 @@ module "asg" {
   #asg_name      = var.asg_name (optional)
   depends_on = [module.asg, module.vpc]
 }  
-  /* 
 
  module "rds" {
     # mysql RDS multi az db cluster
@@ -49,4 +50,4 @@ module "asg" {
     source        = "./aws-modules/s3"
     bucket_name = var.bucket_name
     log_bucket_name = var.log_bucket_name
- } */
+ } 
