174da7ef6f2718ea31108485236e7968f9e0d4b9
el-leston <emanuelleston@gmail.com>
2025-02-05 17:56:52 +0000

    updated workflows and user data

--- challenge-3/aws-modules/asg/user_data.sh
+++ challenge-3/aws-modules/asg/user_data.sh
@@ -3,6 +3,19 @@
 sudo yum update -y
 # Install httpd (Apache)
 sudo yum install -y httpd
+# Install required packages
+sudo yum install -y httpd git
 sudo systemctl start httpd
 sudo systemctl enable httpd
-echo "<html><h1>Welcome to your Apache Web Server!</h1></html>" | sudo tee /var/www/html/index.html
+
+# Clone the Git repository (replace with your repo URL)
+sudo git clone https://github.com/eleston-campus/eleston-campus.github.io.git /var/www/html
+
+# Set proper permissions
+sudo chown -R apache:apache /var/www/html
+sudo chmod -R 755 /var/www/html
+
+# Restart Apache to serve new content
+sudo systemctl restart httpd
+
+
